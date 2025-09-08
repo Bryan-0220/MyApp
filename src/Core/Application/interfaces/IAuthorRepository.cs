d1eceb7d51fbd303394caa0c3d79cd4dd7904d46
Bryan Guano <alejoguano@hotmail.com>
2025-09-08 15:47:03 -0500

    feat: update author repository interface

--- src/Core/Application/interfaces/IAuthorRepository.cs
+++ src/Core/Application/interfaces/IAuthorRepository.cs
@@ -4,5 +4,6 @@ namespace Application.Interfaces
 {
     public interface IAuthorRepository : IRepository<Author>
     {
+        Task<IEnumerable<Author>> FilterAsync(string? name = null, string? nationality = null, System.Collections.Generic.IEnumerable<string>? genres = null, CancellationToken ct = default);
     }
 }
