d69b4f65965da2e610758f6b05e6114738cb685c
Bryan Guano <alejoguano@hotmail.com>
2025-09-09 09:59:55 -0500

    fix: chande Filter funciton

--- src/Core/Application/interfaces/IAuthorRepository.cs
+++ src/Core/Application/interfaces/IAuthorRepository.cs
@@ -5,7 +5,6 @@ namespace Application.Interfaces
 {
     public interface IAuthorRepository : IRepository<Author>
     {
-        // Task<IEnumerable<Author>> FilterAsync(string? name = null, string? nationality = null, System.Collections.Generic.IEnumerable<string>? genres = null, CancellationToken ct = default);
         Task<IEnumerable<Author>> FilterAsync(AuthorFilter? filter = null, CancellationToken ct = default);
     }
 }
--- src/Core/Application/interfaces/IReaderRepository.cs
+++ src/Core/Application/interfaces/IReaderRepository.cs
@@ -4,5 +4,6 @@ namespace Application.Interfaces
 {
     public interface IReaderRepository : IRepository<Reader>
     {
+        Task<IEnumerable<Reader>> FilterAsync(Application.Filters.ReaderFilter? filter = null, CancellationToken ct = default);
     }
 }
